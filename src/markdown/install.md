631e612f5813b6c758c19e9a545d89aa877d1ea2
sunman7 <473910527@qq.com>
2021-01-07 12:54:33 +0800

    æ使用markdown

---
+++ src/markdown/install.md
@@ -0,0 +1,12 @@
+# 安装
+
+打开终端运行下列命令：
+
+```
+npm install vibe-ui //使用npm
+
+yarn add vibe-ui //使用yarn
+```
+
+
+next：[起步](#/doc/get-start)
--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,17 +1,12 @@
 <template>
-    <article class="markdown-body">
-        <h1>安装</h1>
-        <p>打开终端运行下列命令：</p>
-        <pre><code>npm install vibe-ui</code></pre>
-    </article>
+    <article class="markdown-body" v-html="md"></article>
 </template>
+<script>
+    import md from "../markdown/install.md";
 
-<script lang="ts">
-    export default{
-
-    }
+    export default {
+        data() {
+            return {md};
+        }
+    };
 </script>
-
-<style lang="scss" scoped>
-
-</style>
